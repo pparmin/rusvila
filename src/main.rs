079117f8a753709a9c7cd3105bed79afdcf6f633
Philipp Armingeon <philipp.armingeon@aiven.io>
2023-06-17 10:06:37 -0500

    added the foundation for markdown parsing & template handling [NOT FULLY FUNCTIONAL]

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,29 @@
 use std::path::Path;
+
 use clap::{Args, Parser, Subcommand};
 
-use rusvila::{Blog, setup, build};
+use rusvila::{build, remove, setup, Blog};
 
 #[derive(Subcommand, Debug)]
+
 enum Commands {
     Init(Init),
+
+    #[command(about = "Build public files for deployment")]
     Build,
+
+    #[command(about = "Remove blog project (currently for testing purposes)")]
+    Remove,
 }
 
 #[derive(Args, Debug)]
 #[command(about = "initialise a new blog project")]
+
 struct Init {
     #[arg(short = 'n', long = "name")]
     #[arg(required = true)]
     name: String,
+
     #[arg(short = 'p', long = "path")]
     #[arg(required = false)]
     //#[arg(default_value_os_t())]
@@ -26,6 +35,7 @@ struct Init {
 #[command(author = "Philipp Armingeon <philipp.armingeon@googlemail.com>")]
 #[command(version = "1.0")]
 #[command(about = "Rusvila is a tool for quickly deploying static blogs.")]
+
 struct CLI {
     #[command(subcommand)]
     command: Commands,
@@ -33,28 +43,28 @@ struct CLI {
 
 fn main() {
     let cli = CLI::parse();
+
     // project variable is for general tracking of the blog project. Currently, this only allows for one project at once
     let mut project = Blog::new();
+
     match &cli.command {
-        Commands::Init(init) => {
-            match &init.path {
-                Some(p) => {
-                    setup(&init.name, Path::new(&p));
-                    project.name = init.name.clone();
-                    project.location = Path::new(&p).to_path_buf();
-                },
-                None => {
-                    let path = std::env::current_dir()
-                        .expect("Error while fetching current working directory");
-                    setup(&init.name, &path);
-                    project.name = init.name.clone();
-                    project.location = path.to_path_buf();
-                }
+        Commands::Init(init) => match &init.path {
+            Some(p) => {
+                setup(&init.name, Path::new(&p));
+                project.name = init.name.clone();
+                project.location = Path::new(&p).to_path_buf();
+            }
+
+            None => {
+                let path = std::env::current_dir()
+                    .expect("Error while fetching current working directory");
+                setup(&init.name, &path);
+                project.name = init.name.clone();
+                project.location = path.to_path_buf();
             }
         },
-        Commands::Build => {
-           build(project.location) 
-        }
-    }
+        Commands::Build => build(project.location),
 
+        Commands::Remove => remove(project.location),
+    }
 }
--- src/util/lib.rs
+++ src/util/lib.rs
@@ -1,72 +1,105 @@
 use std::path::{Path, PathBuf};
 //use std::fs::{File, create_dir_all};
+use handlebars::{template, Handlebars};
+use markdown;
 
-static INDEX_MD: &str = 
-"<!-- template: homepage -->
+static INDEX_MD: &str = "<!-- template: homepage -->
 # Hello World
 ";
 
-static MAIN_JS: &str = 
-"console.log('hello world')
+static MAIN_JS: &str = "console.log('hello world')
 ";
 
-static DEFAULT: &str = 
-"<html>
-  <body>
-    <div id='page-content'></div>
-  </body>
+static DEFAULT: &str = "<html>
+<body>
+<div id='page-content'></div>
+</body>
 </html>
 ";
 
-static HOMEPAGE: &str = 
-"<html>
-  <body>
-    <p>My first Teeny page</p>
-    <div id='page-content'></div>
-    <script src='main.js' />
-  </body>
+static HOMEPAGE: &str = "<html>
+<body>
+<p>My first Teeny page</p>
+<div id='page-content'></div>
+<script src='main.js' />
+</body>
 </html>
 ";
 
 pub struct Blog {
-  pub name: String,
-  pub location: PathBuf,
+    pub name: String,
+    pub location: PathBuf,
 }
 
 impl Blog {
-  pub fn new() -> Self {
-    Blog {
-      name: String::new(), 
-      location: std::env::current_dir().unwrap(),
+    pub fn new() -> Self {
+        Blog {
+            name: String::new(),
+            location: std::env::current_dir().unwrap(),
+        }
     }
-  }
 }
 
-
 pub fn setup(name: &String, path: &Path) {
-    let root_dir = format!("{}/{}", path.display(), name);
-    println!("Creating root directory for project {} in the following directory: {}", name, path.display());
+    let mut reg = Handlebars::new();
+
+    let root_dir = format!(
+        "{}/{}",
+        path.display().to_string().trim_end_matches("/"),
+        name
+    );
+
+    println!(
+        "Creating root directory for project {} in the following directory: {}",
+        name,
+        path.display()
+    );
+
     println!("Root path: {}", root_dir);
     std::fs::create_dir_all(Path::new(&root_dir)).expect("Error while creating root directory");
-    
+
     let pages_dir = format!("{}/pages", root_dir);
     std::fs::create_dir(&pages_dir).expect("Error while creating pages directory");
+
     let index_file = format!("{}/index.md", &pages_dir);
     std::fs::write(index_file, INDEX_MD).expect("error while creating index.md in pages directory");
 
     let static_dir = format!("{}/static", root_dir);
     std::fs::create_dir(&static_dir).expect("Error while creating static directory");
+   
     let main_js = format!("{}/main.js", &static_dir);
     std::fs::write(main_js, MAIN_JS).expect("error while creating main.js in static directory");
 
     let templates_dir = format!("{}/templates", root_dir);
     std::fs::create_dir(&templates_dir).expect("Error while creating templates directory");
+
     let default_html = format!("{}/default.html", &templates_dir);
     let homepage_html = format!("{}/homepage.html", &templates_dir);
-    std::fs::write(default_html, DEFAULT).expect("error while creating default.html in templates directory");
-    std::fs::write(homepage_html, HOMEPAGE).expect("error while creating homepage.html in templates directory");
+    std::fs::write(default_html, DEFAULT)
+        .expect("error while creating default.html in templates directory");
+    std::fs::write(homepage_html, HOMEPAGE)
+        .expect("error while creating homepage.html in templates directory");
+
+    reg.register_templates_directory(".html", templates_dir)
+        .expect("error while registering templates/ directory");
+
+    assert!(
+        reg.has_template("default"),
+        "default.html template not found in templates/"
+    );
+
+    assert!(
+        reg.has_template("homepage"),
+        "homepage.html template not found in templates/"
+    );
 }
 
 pub fn build(project_root: PathBuf) {
-  todo!()
+    println!("{}", markdown::to_html(INDEX_MD))
+}
+
+pub fn remove(project_root: PathBuf) {
+    //std::fs::remove_dir_all(project_root.clone()).expect("Error while removing project location");
+
+    println!("project at {}", project_root.display())
 }
